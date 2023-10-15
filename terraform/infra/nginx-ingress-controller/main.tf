9902bc32c29a6fed4b997b192608ed7e4a286923
milagre <milagre@users.noreply.github.com>
2023-10-15 10:36:22 -0400

    [TF] Nginx resources and affinity

--- terraform/infra/nginx-ingress-controller/main.tf
+++ terraform/infra/nginx-ingress-controller/main.tf
@@ -10,12 +10,65 @@ resource "helm_release" "nginx" {
   version    = "4.7.2"
 
   dynamic "set" {
-    for_each = var.env.is_local ? {
-      "controller.service.type" = "NodePort"
-    } : {}
+    for_each = merge(
+      var.env.is_local ? {
+        "controller.service.type" = "NodePort"
+      } : {},
+      {
+        "controller.replicaCount" = 2
+
+        "controller.autoscaling.enabled"                           = true
+        "controller.autoscaling.minReplicas"                       = 2
+        "controller.autoscaling.targetCPUUtilizationPercentage"    = 75
+        "controller.autoscaling.targetMemoryUtilizationPercentage" = 75
+
+        "controller.resources.requests.cpu"    = "200m"
+        "controller.resources.requests.memory" = "196Mi"
+        "controller.resources.limits.cpu"      = "250m"
+        "controller.resources.limits.memory"   = "256Mi"
+      }
+    )
     content {
       name  = set.key
       value = set.value
+    }
+  }
+
+  values = [
+    yamlencode({ controller = { affinity = local.affinity } })
+  ]
+}
+
+locals {
+  affinity = {
+    podAntiAffinity = {
+      preferredDuringSchedulingIgnoredDuringExecution = [
+        {
+          weight = 100
+          podAffinityTerm = {
+            labelSelector = {
+              matchExpressions = [
+                {
+                  key      = "app.kubernetes.io/name"
+                  operator = "In"
+                  values   = ["ingress-nginx"]
+                },
+                {
+                  key      = "app.kubernetes.io/instance"
+                  operator = "In"
+                  values   = ["ingress-nginx"]
+                },
+                {
+                  key      = "app.kubernetes.io/component"
+                  operator = "In"
+                  values   = ["controller"]
+                },
+              ]
+            }
+            topologyKey = "kubernetes.io/hostname"
+          }
+        },
+      ]
     }
   }
 }
