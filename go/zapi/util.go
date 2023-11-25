59f0d4c57303c75e8bdad237d24f77a47a3f2e45
milagre <milagre@users.noreply.github.com>
2023-11-25 13:10:25 -0500

    [Go] zapi.Pathf

--- go/zapi/util.go
+++ go/zapi/util.go
@@ -1,9 +1,20 @@
 package zapi
 
+import "fmt"
+
 func AppendRoutes(routelists ...[]Route) []Route {
 	result := []Route{}
 	for _, routes := range routelists {
 		result = append(result, routes...)
 	}
 	return result
+}
+
+func Pathf(template string, params ...string) string {
+	parts := make([]interface{}, len(params))
+	for i, p := range params {
+		parts[i] = fmt.Sprintf("{%s}", p)
+	}
+
+	return fmt.Sprintf(template, parts...)
 }
