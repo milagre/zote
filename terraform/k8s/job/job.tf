49e0de4dee9ecefd0afad5082effce3b18c1a3f2
milagre <milagre@users.noreply.github.com>
2025-08-04 14:48:42 -0400

    [TF] Kubernetes job

---
+++ terraform/k8s/job/job.tf
@@ -0,0 +1,100 @@
+
+locals {
+  timestamptag = replace(timestamp(), "/[-:TZ]/", "")
+  tag          = var.tag == "latest" ? "latest-${local.timestamptag}" : var.tag
+}
+
+resource "kubernetes_job" "job" {
+  metadata {
+    name      = var.name
+    namespace = var.namespace
+    labels = {
+      app     = var.name
+      version = local.tag
+    }
+  }
+
+  spec {
+    template {
+      metadata {
+        name = var.name
+        labels = {
+          app     = var.name
+          version = local.tag
+        }
+      }
+      spec {
+        container {
+          name = var.name
+
+          image = "${var.image}:${var.tag}"
+
+          image_pull_policy = var.env.is_local ? "Never" : "IfNotPresent"
+
+          command = var.cmd
+          args    = var.args
+
+          resources {
+            limits = {
+              cpu    = module.profile.cpu_cores.max
+              memory = "${module.profile.mem_mb.max}M"
+            }
+            requests = {
+              cpu    = module.profile.cpu_cores.min
+              memory = "${module.profile.mem_mb.min}M"
+            }
+          }
+
+          // Attach configmaps to environment
+          dynamic "env_from" {
+            for_each = coalesce(var.conf.configmaps, [])
+            content {
+              config_map_ref {
+                name = env_from.value
+              }
+            }
+          }
+
+          // Attach secrets to environment
+          dynamic "env_from" {
+            for_each = coalesce(var.conf.secrets, [])
+            content {
+              secret_ref {
+                name = env_from.value
+              }
+            }
+          }
+
+          // Attach arbitrary env values
+          dynamic "env" {
+            for_each = coalesce(var.conf.values, {})
+            content {
+              name  = env.key
+              value = env.value
+            }
+          }
+
+          // Mount configmaps needed for files in container
+          dynamic "volume_mount" {
+            for_each = coalesce(var.files.configmaps, {})
+            content {
+              name       = volume_mount.value
+              mount_path = volume_mount.key
+            }
+          }
+        }
+
+        // Attach configmaps needed for files to spec
+        dynamic "volume" {
+          for_each = coalesce(var.files.configmaps, {})
+          content {
+            name = volume.value
+            config_map {
+              name = volume.value
+            }
+          }
+        }
+      }
+    }
+  }
+}
---
+++ terraform/k8s/job/main.tf
@@ -0,0 +1,40 @@
+variable "env" {}
+variable "name" {}
+variable "namespace" {}
+variable "image" {}
+variable "tag" {}
+variable "conf" {
+  type = object({
+    configmaps = optional(list(string))
+    secrets    = optional(list(string))
+    values     = optional(map(string))
+  })
+  default = {}
+}
+variable "files" {
+  type = object({
+    configmaps = map(string),
+  })
+  default = {
+    configmaps = {},
+  }
+}
+variable "profile" {}
+variable "cmd" {
+  type    = list(string)
+  default = null
+}
+variable "args" {
+  type    = list(string)
+  default = null
+}
+
+module "profile" {
+  source = "./../../structs/profile"
+
+  profile = var.profile
+}
+
+locals {
+}
+
