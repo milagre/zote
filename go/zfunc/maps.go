1bfa982b6a1867209cd3fa78612ec713e77975bb
Mike Taczak <milagre@users.noreply.github.com>
2025-12-22 10:25:19 -0500

    [Go] zfunc: GroupBy

--- go/zfunc/maps.go
+++ go/zfunc/maps.go
@@ -9,3 +9,13 @@ func IndexBy[T any, U comparable](list []T, cb func(T) U) map[U]T {
 
 	return result
 }
+
+func GroupBy[T any, U comparable](list []T, cb func(T) U) map[U][]T {
+	result := map[U][]T{}
+
+	for _, elem := range list {
+		result[cb(elem)] = append(result[cb(elem)], elem)
+	}
+
+	return result
+}
