b755105661730500070630fe29060a0e0381a60f
Mike Taczak <milagre@users.noreply.github.com>
2026-01-21 08:12:38 -0500

    [TF] config env var collection passthrough

--- terraform/common/config/main.tf
+++ terraform/common/config/main.tf
@@ -3,6 +3,10 @@ terraform {
 }
 
 variable "env" {}
+variable "vars" {
+  default = {}
+  type    = map(string)
+}
 
 module "read" {
   source = "./read"
@@ -20,4 +24,8 @@ output "raw" {
 
 output "data" {
   value = module.read.data
+}
+
+output "env_vars" {
+  value = var.vars
 }
